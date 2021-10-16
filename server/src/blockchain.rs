66369d81cf535a331cb06d26271bbe9f458c4e07
TheHackerCoding <triniziah@gmail.com>
2021-10-15 21:11:28 -0400

    got some amazing help from the rust discord server to help with some rust compiler errors

--- server/src/blockchain.rs
+++ server/src/blockchain.rs
@@ -1,11 +1,15 @@
-#[allow(non_camel_case_types)]
+#![allow(non_camel_case_types)]
 use std::collections::HashSet;
 use serde::{Serialize, Deserialize};
 use chrono::Utc;
 use sha256::digest;
 
 
-#[derive(Serialize, Deserialize, Debug)]
+fn timestamp() -> i64 {
+  return Utc::now().timestamp();
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BlockData {
   pub name: String,
   pub data: String,
@@ -17,7 +21,7 @@ pub struct NewBlockOptions {
   pub previousHash: Option<String>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
   pub index: i64,
   pub hash: String,
@@ -26,7 +30,7 @@ pub struct Block {
   pub previousHash: String
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Blockchain {
   pub chain: Vec<Block>,
   pub currentData: Vec<BlockData>,
@@ -48,22 +52,26 @@ impl Blockchain {
   }
 
   pub fn lastBlock(&self) -> &Block {
-    return &self.chain[self.chain.len() - 1];
+    return self.chain.last().unwrap();
   }
 
-  pub fn newBlock(&mut self, option: NewBlockOptions) -> &Block {
+  pub fn newBlock(&mut self, option: NewBlockOptions) -> Block {
     let uhh: String = match option.previousHash {
       Some(ans) => ans,
-      None => self.lastBlock().hash
+      None => self.lastBlock().hash.clone()
     };
-    let _block = Block {
+    let mut _block = Block {
       index: self.chain.len() as i64,
       hash: "".to_string(),
-      timestamp: Utc::now().timestamp(),
-      data: self.currentData,
+      timestamp: timestamp(),
+      data: self.currentData.clone(),
       previousHash: uhh
     };
-    _block.hash = digest(String::from("{}", _block.index.to_string() + &_block.previousHash + &serde_json::to_string(&_block).unwrap()));
-    &_block
+    // _block.hash = digest(String::from("{}", _block.index.to_string() + &_block.previousHash + &serde_json::to_string(&_block).unwrap()));
+    _block.hash = digest(format!("{}{}{}", _block.index, _block.previousHash, serde_json::to_string(&_block).unwrap()));
+    self.currentData.clear();
+    let __block = _block.clone();
+    self.chain.push(__block);
+    _block
   }
 }
